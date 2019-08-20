5dcbe259e47e71d0f2d00623231cc2d78dfdfe25
jmhrovat <jmhrovat@gmail.com>
2019-08-20 09:28:53 -0700

    User registration form completed

--- crossconnect/users/views.py
+++ crossconnect/users/views.py
@@ -1,9 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CustomUserCreationForm
+from django.contrib.auth import authenticate, login
+from django import forms
 
 # Create your views here.
 def register(request):
+    print(request)
+    if request.method == "POST":
+        print('Method is Post')
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            print("Form is valid")
+            form.save()
+            new_user = authenticate(email=form.cleaned_data['email'],
+                                    password=form.cleaned_data['password1'],
+                                    )
+            login(request, new_user)
+            return redirect('homepage')
+        else:
+            print(form.errors)
+    else:
+        print("Not valid")
+        form = CustomUserCreationForm()
 
-    return render(request, 'users/register.html')
+    context = {
+    'form': form
+    }
+
+    return render(request, 'users/register.html', context)
 
 def profile(request):
     return
