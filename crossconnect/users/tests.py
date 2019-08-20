13614bba073605e0c38d944c5e24762e5c6939a8
jmhrovat <jmhrovat@gmail.com>
2019-08-19 18:28:06 -0700

    Added tests for Users

--- crossconnect/users/tests.py
+++ crossconnect/users/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
 # Create your tests here.
+
+User = get_user_model()
+
+class CustomUserTestCase(TestCase):
+
+    def test_create_user(self):
+        data_dict = {
+            'email':'test@email.com',
+            'password': 'password',
+            'first_name': 'John',
+            'last_name': 'Doe',
+            'is_pastor': 'True'
+        }
+        user = User.objects.create(**data_dict)
+        self.assertEqual(user.pk, user.id)
