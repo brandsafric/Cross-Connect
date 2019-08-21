5313024decd3a72f5d63dc526f9a177d417165ed
jmhrovat <jmhrovat@gmail.com>
2019-08-20 21:22:54 -0700

    Added the Service Template model

--- crossconnect/church/tests/test_models.py
+++ crossconnect/church/tests/test_models.py
@@ -1,16 +1,30 @@
 from django.test import TestCase
-from church.models import Church
+from church.models import Church, ServiceTemplate
 
 class ChurchTestCase(TestCase):
 
-    def test_create_church(self):
-        """
-        Tests whether Church model is functional
-        """
+    def _create_church(self):
         data_dict = {
             'name':'Test Church',
             'city': 'Redlands',
             'state': 'California'
         }
-        church = Church.objects.create(**data_dict)
+        return Church.objects.create(**data_dict)
+
+    def _create_service_template(self, church):
+        data_dict = {
+            'church': church,
+            'name': 'Service A',
+            'time': "08:00:00",
+            'day': 0
+        }
+        return ServiceTemplate.objects.create(**data_dict)
+
+    def test_create_church(self):
+        church = self._create_church()
         self.assertEqual(church.pk, church.id)
+
+    def test_create_service_template(self):
+        church = self._create_church()
+        service = self._create_service_template(church)
+        self.assertEqual(service.pk, service.id)
