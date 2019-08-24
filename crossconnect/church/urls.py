1eca25605d44ebd6ea1334353d204ff7b274a16a
jmhrovat <jmhrovat@gmail.com>
2019-08-23 23:12:37 -0700

    Finished mobile dashboard

--- crossconnect/church/urls.py
+++ crossconnect/church/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('services/add', views.add_service_template, name='add_service_template'),
     path('service/add', views.add_service, name='add_service'),
     path('services/<int:id>', views.service_template, name='service_template'),
+    path('service/<int:id>', views.service_detail, name='service_detail')
+
 ]
