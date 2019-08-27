e72444082034135b4a438082473bc6e7aba2cafb
jmhrovat <jmhrovat@gmail.com>
2019-08-26 21:59:18 -0700

    Chart js

--- crossconnect/church/functions.py
+++ crossconnect/church/functions.py
@@ -9,10 +9,20 @@ def get_service_count_date_range_average(days, services):
     average = date_range_services.aggregate(Avg('attendance_count'))
     return int(average['attendance_count__avg'])
 
-def get_service_count_date_range_change(days, services):
+def get_service_count_date_range_change(services):
     today = datetime.now()
     date_delta = today + timedelta(-30)
     date_range_services = services.filter(date__range=[date_delta, today])
     recent_service = date_range_services[(date_range_services.count() - 1)]
-    latest_service = date_range_services[0]
-    return latest_service.attendance_count - recent_service.attendance_count
+    oldest_service = date_range_services[0]
+    return recent_service.attendance_count - oldest_service.attendance_count
+
+def get_services_date_range(services, first_date, second_date):
+    return services.filter(date__range=[second_date, first_date])
+
+def get_services_count_data(services):
+    data = list(services.values_list('date', 'attendance_count'))
+    data = list(map(list, data))
+    for set in data:
+        set[0] = set[0].strftime('%m/%d/%y')
+    return data
