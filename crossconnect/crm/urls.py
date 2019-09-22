491cd3059e2cb947a57392230249bae48e737582
jmhrovat <jmhrovat@gmail.com>
2019-09-21 21:01:54 -0700

    Added contact detail page

--- crossconnect/crm/urls.py
+++ crossconnect/crm/urls.py
@@ -3,5 +3,6 @@ from crm import views
 
 urlpatterns = [
     path('', views.contacts, name='contacts'),
+    path('<int:id>', views.contact_detail, name='contact_detail'),
     path('add', views.add_contact, name='add_contact')
 ]
